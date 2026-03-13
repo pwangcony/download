778dd702e8beffc690ff51006c6ee6606213c21f
퐝코니 <91484435+pwangcony@users.noreply.github.com>
2026-03-13 16:16:33 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,10 +1,10 @@
 const downloadDatabase = [
     // SAPH660 Data
     { model: "SAPH660", title: "Intel Chipset Device Software", type: "Driver", size: "4.48 MB", ver: "10.1.19376.8374", 
-     url: "https://www.dropbox.com/scl/fi/vxtrkssi2lupzwec4su3n/Chipset-10.1.19376.8374-Public-MUP.zip?rlkey=3cr1416bj03tekdbzvuy0q8zu&st=izkdr7j8&dl=0", os: "Win10/11" },
+     url: "https://www.dropbox.com/scl/fi/vxtrkssi2lupzwec4su3n/Chipset-10.1.19376.8374-Public-MUP.zip?rlkey=3cr1416bj03tekdbzvuy0q8zu&st=izkdr7j8&dl=1", os: "Win10/11" },
     { model: "SAPH660", title: "Intel Grphics Driver", type: "Driver", size: "617 MB", ver: "101.4369", 
-     url: "https://www.dropbox.com/scl/fi/rcvfhsa5hsewghevhyp4l/gfx_win_101.4369.zip?rlkey=jxsbcucqmxrmzbu64f6wmvmpc&st=ihsy0awy&dl=0", os: "Win10" },
+     url: "https://www.dropbox.com/scl/fi/rcvfhsa5hsewghevhyp4l/gfx_win_101.4369.zip?rlkey=jxsbcucqmxrmzbu64f6wmvmpc&st=ihsy0awy&dl=1", os: "Win10" },
     { model: "SAPH660", title: "Intel Grphics Driver", type: "Utility", size: "884 MB", ver: "101.5972", 
-     url: "https://www.dropbox.com/scl/fi/tvu5fpfznagi8pabg8xu7/gfx_win_101.5972.exe?rlkey=sds04irpvv0o7xe08azizrw08&st=0c40menb&dl=0", os: "Win11" },
+     url: "https://www.dropbox.com/scl/fi/tvu5fpfznagi8pabg8xu7/gfx_win_101.5972.exe?rlkey=sds04irpvv0o7xe08azizrw08&st=0c40menb&dl=1", os: "Win11" },
 
 ];
